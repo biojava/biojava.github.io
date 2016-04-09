283f6b5903b2141561450d0f8bfcb53e60888263
Andreas Prlic <andreas.prlic@gmail.com>
2016-04-09 11:35:28 -0700

    convert

--- convert.sh
+++ convert.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-now="$(date +'%Y%m%d')"
-echo $now
-branchname="master_$now"
-echo $branchname
+#now="$(date +'%Y%m%d')"
+#echo $now
+#branchname="master_$now"
+#echo $branchname
 
 #git checkout -b $branchname master    # create and switch to the release branch
 #git push -u origin $branchname        # push the release branch to the remote and track it
@@ -12,9 +12,9 @@ echo $branchname
 # perform conversion
 #../mediawiki_to_git_md/convert.py mediawiki_dump.xml
 
-#git branch -d master              # delete local master
-#git push --delete origin master   # delete remote master
-#git remote prune origin           # delete the remote tracking branch
+git branch -d master              # delete local master
+git push --delete origin master   # delete remote master
+git remote prune origin           # delete the remote tracking branch
 
-#git branch -m master              # rename the local branch to master
-#git push origin master            # make the new master branch remote
+git branch -m master              # rename the local branch to master
+git push origin master            # make the new master branch remote
